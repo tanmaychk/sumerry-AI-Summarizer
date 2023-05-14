8ab01fb6e0f6bc200149e3356aceccd76c599cd5
tanmaychk <tanmay23c@gmail.com>
2023-05-14 18:38:26 +0530

    ✅Final Changes

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,21 +1,58 @@
 import React from 'react'
 import { useState,useEffect } from 'react'
 import { copy,linkIcon,loader,tick } from '../assets'
-
+import { useLazyGetSummaryQuery } from '../services/article'
 
 const Demo = () => {
 
-  const [article, setarticle] = useState({
+const [article, setarticle] = useState({
     url:"",
     summary:"",
   });
 
+const [allArticles, setallArticles] = useState([])
+
+
+const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+const [copied, setCopied] = useState("")
+
+useEffect(() => {
+  const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem('articles')
+    )
+    if(articlesFromLocalStorage){
+     setallArticles(articlesFromLocalStorage)
+    }
+  }, []);
+ 
+
   const handleSubmit = async(e) =>{
-    alert("submitted");
+    e.preventDefault();
+    const{data} = await getSummary({articleUrl: article.url});
+
+    if(data?.summary){
+      const newArticle = {...article,summary:data.summary};
+      const updatedAllArticles = [newArticle,...allArticles];
+
+      setarticle(newArticle)
+      setallArticles(updatedAllArticles)
+
+      localStorage.setItem('articles',JSON.stringify(updatedAllArticles))
+    }
   }
 
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  };
 
-
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      handleSubmit(e);
+    }
+  };
 
   return (
     <section className='mt-16 w-ful max-w-xl'>
@@ -30,6 +67,7 @@ const Demo = () => {
             placeholder="Enter a URL"
             value={article.url}
             onChange = {(e)=>setarticle({...article,url:e.target.value})}
+            onKeyDown={handleKeyDown}
             required
             className='url_input peer'
           />
@@ -39,10 +77,54 @@ const Demo = () => {
         </form>
 
         {/*Browse URL Hitory*/}
+        <div className='flex flex-col gap-1 max-h-60
+          overflow-y-auto'>
+          {allArticles.map((item,index)=>(
 
+          <div key = {`link -${index}`}
+              onClick={()=>setarticle(item)}
+              className='link_card'
+            >
+              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
+                <img 
+                  src={copied === item.url ? tick : copy}
+                  alt={copied === item.url ? "tick_icon" : "copy_icon"}
+                  className='w-[40%] h-[40%] object-contain'/>
+              </div>
+              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                {item.url}
+              </p>
+          </div>
+          ))}
+        </div>
       </div>
        {/*Display Result*/}
-      
+       <div className='my-10 max-w-full flex justify-center items-center'>
+        {isFetching ? (
+          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+        ) : error ? (
+          <p className='font-inter font-bold text-black text-center'>
+            Well, that wasn't supposed to happen...
+            <br />
+            <span className='font-satoshi font-normal text-gray-700'>
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                Article <span className='blue_gradient'>Summary</span>
+              </h2>
+              <div className='summary_box'>
+                <p className='font-inter font-medium text-sm text-gray-700'>
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
         <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-            <img src={logo} alt = "sumz_logo" className='w-28' object-contain/>
+            <img src={logo} alt = "logo" className='w-28' object-contain/>
             <button type="button" onClick={()=>window.open('https://github.com/tanmaychk')} className="black_btn">Github</button>
         </nav>
 
